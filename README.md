9267a91c2a6aee0fcd8bba36ea393a1799b42b16
bbbottle <b@bbki.ng>
2025-01-05 09:21:18 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Extism Rust PDK Plugin
+A now page plugin.
 
-See more documentation at https://github.com/extism/rust-pdk and
-[join us on Discord](https://extism.org/discord) for more help.
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+#[derive(Serialize, Deserialize, ToBytes, FromBytes)]
+#[encoding(Json)]
+struct Payload {
+    pub content: String,
+}
+
+#[host_fn("extism:host/user")]
+extern "ExtismHost" {
+    fn toast(content: String);
+}
 
 // start with something simple
 #[plugin_fn]
 pub fn greet(name: String) -> FnResult<String> {
-    Ok(format!("Hello, {}!", name))
+    unsafe {
+        let _ = toast(format!("Got you, {}!", name));
+    };
+    let c = format!("Hey, *{}*!", name);
+    Ok(markdown::to_html(&c))
 }
 
 // use json data for inputs and outputs
@@ -18,6 +34,23 @@ struct Add {
 #[encoding(Json)]
 struct Sum {
     value: i32,
+}
+
+#[plugin_fn]
+pub fn http_get(Json(req): Json<HttpRequest>) -> FnResult<Memory> {
+    trace!("HTTP Request: {:?}", req);
+    info!("Request to: {}", req.url);
+
+    let api = "https://api.bbki.ng/movies";
+
+    let req_new = HttpRequest{
+        url: (&api).to_string(),
+        method: Some("GET".to_string()),
+        headers: BTreeMap::new(),
+    };
+
+    let res = http::request::<()>(&req_new, None)?;
+    Ok(res.into_memory())
 }
 
 #[plugin_fn]
